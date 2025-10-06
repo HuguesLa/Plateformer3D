4eb2eaf3a89782d54b1cf43c2f09682451eb7b96
Mathieu-Da-Costa <mathieu.dacosta@etudiant.univ-brest.fr>
2025-10-06 17:02:28 +0200

    Crane mouvent

---
+++ Assets/Script/AITarget.cs
@@ -0,0 +1,27 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class AITarget : MonoBehaviour
+{
+    public Transform Target;
+
+    private NavMeshAgent m_Agent;
+    private float m_Distance;
+
+
+    void Start()
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    void Update()
+    {
+            m_Agent.destination = Target.position;
+        
+
+    }
+}
